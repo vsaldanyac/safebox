f6e2c98c8d1b23cd0c9930d9f962147232ad0e34
vsaldanya <victor.saldanya@fav24.com>
2020-11-07 17:03:20 +0100

    Create Post CreateSafebox on controller with SafeboxDTO

--- src/main/java/com/securitish/safebox/com/controller/beta/SafeboxController.java
+++ src/main/java/com/securitish/safebox/com/controller/beta/SafeboxController.java
@@ -1,9 +1,8 @@
 package com.securitish.safebox.com.controller.beta;
 
+import com.securitish.safebox.com.dto.SafeboxDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
   @RequestMapping("/safebox")
@@ -13,5 +12,11 @@ public class SafeboxController {
   public ResponseEntity<?> testEndpoint() {
 
     return ResponseEntity.ok("Service is up!");
+  }
+
+  @PostMapping
+  public ResponseEntity<?> postNewSafeBox(@RequestBody final SafeboxDTO safebox) {
+
+    return ResponseEntity.ok(safebox);
   }
 }
---
+++ src/main/java/com/securitish/safebox/com/dto/SafeboxDTO.java
@@ -0,0 +1,12 @@
+package com.securitish.safebox.com.dto;
+
+import lombok.Data;
+
+@Data
+public class SafeboxDTO {
+
+  String id;
+  String name;
+  String password;
+
+}
