d223b1e0fe2a151330b698b27659558abf9db288
vsaldanya <victor.saldanya@fav24.com>
2020-11-08 01:05:05 +0100

    Get all the DTO info, but only shows the id

--- src/main/java/com/securitish/safebox/com/dto/SafeboxDTO.java
+++ src/main/java/com/securitish/safebox/com/dto/SafeboxDTO.java
@@ -1,13 +1,15 @@
 package com.securitish.safebox.com.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class SafeboxDTO {
 
   String id;
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   String name;
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   String password;
-
 
 }
