5d4f162b4ce350fcd1864558287f24f9201ac31f
vsaldanya <victor.saldanya@fav24.com>
2020-11-08 05:02:50 +0100

    Version 1 of the controller overriding the older one

---
+++ src/main/java/com/securitish/safebox/com/controller/v1/SafeboxController_v1.java
@@ -0,0 +1,61 @@
+package com.securitish.safebox.com.controller.v1;
+
+import com.securitish.safebox.com.controller.beta.SafeboxController;
+import com.securitish.safebox.com.service.SafeboxService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.securitish.safebox.com.util.SecurityUtils.getJWTToken;
+
+@RestController
+@RequestMapping("v1/safebox")
+public class SafeboxController_v1 extends SafeboxController {
+
+  @Autowired
+  SafeboxService safeboxService;
+
+
+  @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> getSafebox(@PathVariable String id,
+                                      @RequestHeader(name = "X-Auth-Name") String authName,
+                                      @RequestHeader(name = "X-Auth-Pwd") String authPwd) {
+    safeboxService.validateUserForGiveSafebox(id, authName, authPwd);
+    String token = getJWTToken(authName);
+    return ResponseEntity.ok(new StringResponse(token));
+  }
+
+  @Override
+  @PutMapping("/{id}/items")
+  public ResponseEntity<?> putItemsFromSafebox(@PathVariable String id,
+                                               @RequestBody final List<String> safeboxItems,
+                                               @RequestHeader(name = "X-Auth-Name", required = false) String authName,
+                                               @RequestHeader(name = "X-Auth-Pwd", required = false) String authPwd) {
+    safeboxService.putItemsOnSafebox(id, safeboxItems);
+    return ResponseEntity.ok().build();
+  }
+
+  @Override
+  @GetMapping("/{id}/items")
+  public ResponseEntity<?> getItemsFromSafebox(@PathVariable String id,
+                                               @RequestHeader(name = "X-Auth-Name", required = false) String authName,
+                                               @RequestHeader(name = "X-Auth-Pwd", required = false) String authPwd) {
+    return ResponseEntity.ok(safeboxService.getItemsFromSafebox(id));
+  }
+
+  class StringResponse {
+
+    private String token;
+
+    public StringResponse(String s) {
+      this.token = s;
+    }
+
+    public String getToken() {
+      return token;
+    }
+  }
+}
