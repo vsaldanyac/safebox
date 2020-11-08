78066465d53f1db57063c7a37cd13fc0ec3f6b89
vsaldanya <victor.saldanya@fav24.com>
2020-11-08 01:16:32 +0100

    Store password encrypted

--- src/main/java/com/securitish/safebox/com/dto/mapper/SafeboxMapper.java
+++ src/main/java/com/securitish/safebox/com/dto/mapper/SafeboxMapper.java
@@ -5,6 +5,7 @@ import com.securitish.safebox.com.dto.SafeboxDTO;
 import com.securitish.safebox.com.repository.dao.SafeboxDAO;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -15,6 +16,7 @@ public interface SafeboxMapper {
   @InheritInverseConfiguration
   Safebox map(SafeboxDTO safeboxDTO);
 
+  @Mapping(target="password", expression = "java(org.apache.commons.codec.digest.DigestUtils.md5Hex(safeboxDTO.getPassword()))")
   SafeboxDAO mapDAO(SafeboxDTO safeboxDTO);
 
   @InheritInverseConfiguration
