d8a0436bf1365fe61d7427ad7ae39ab2cb52282c
vsaldanya <victor.saldanya@fav24.com>
2020-11-08 01:03:53 +0100

    Enable JPA repositories

--- src/main/java/com/securitish/safebox/com/SafeboxApplication.java
+++ src/main/java/com/securitish/safebox/com/SafeboxApplication.java
@@ -3,8 +3,10 @@ package com.securitish.safebox.com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 @EntityScan
 public class SafeboxApplication {
 
