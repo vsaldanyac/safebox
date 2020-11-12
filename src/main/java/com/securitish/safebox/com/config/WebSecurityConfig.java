4c2d8e7c4a3724653719f86c955342c5ebe2445a
vsaldanya <victor.saldanya@fav24.com>
2020-11-12 09:50:50 +0100

    Give access to swagger and h2-console

--- src/main/java/com/securitish/safebox/com/config/WebSecurityConfig.java
+++ src/main/java/com/securitish/safebox/com/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.securitish.safebox.com.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -34,6 +35,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .anyRequest().authenticated();
 
     http.headers().frameOptions().disable();
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/v2/api-docs/**");
+    web.ignoring().antMatchers("/swagger.json");
+    web.ignoring().antMatchers("/swagger-ui.html");
+    web.ignoring().antMatchers("/swagger-resources/**");
+    web.ignoring().antMatchers("/webjars/**");
+    web.ignoring().antMatchers("/h2-console/**");
   }
 
 }
